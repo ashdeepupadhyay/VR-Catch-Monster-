0f9bf7cd326cf15930285c6a72df11da9a5ddd33
ashdeep <ashdeepupadhyay@gmail.com>
2018-12-01 11:56:27 +0530

    initial scene setup and shoot poke ball

---
+++ VR_Catch_Monster/Assets/Scripts/WallkAndShoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallkAndShoot : MonoBehaviour {
+
+    public GameObject pokeball;
+    public float shootSpeed = 10;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            var pokego= Instantiate(pokeball);
+            pokego.transform.position = gameObject.transform.position;
+            Rigidbody rb = pokego.GetComponent<Rigidbody>();
+            Camera cam = GetComponentInChildren<Camera>();
+            rb.velocity = cam.transform.rotation * Vector3.forward *shootSpeed;
+        }
+
+
+    }
+}
