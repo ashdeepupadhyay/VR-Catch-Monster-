fd5a4119850d214d64835292309d4d51655b7556
ashdeep <ashdeepupadhyay@gmail.com>
2018-12-01 17:52:46 +0530

    adding monstors and making of gameplay

---
+++ VR_Catch_Monster/Assets/Scripts/CollectMonsters.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectMonsters : MonoBehaviour {
+
+    MCalculator mcalc;
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("redmonster")|| other.CompareTag("bluemonster")|| other.CompareTag("violetmonster")|| other.CompareTag("yellowmonster"))
+        {
+            mcalc.score++;
+            mcalc.DisplayScore();
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+        
+        
+    }
+    public void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.CompareTag("ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Use this for initialization
+    void Start () {
+        mcalc = FindObjectOfType<MCalculator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ VR_Catch_Monster/Assets/Scripts/MCalculator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MCalculator : MonoBehaviour {
+
+    public int score;
+    public TextMesh totalMonster;
+    public float timeRemaining;
+    public TextMesh timerText;
+    public void DisplayScore()
+    {
+        totalMonster.text = score.ToString();
+        //Debug.Log("the score is " + score);
+    }
+	// Use this for initialization
+	void Start () {
+        totalMonster.text = score.ToString();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //DisplayScore();
+        timeRemaining -=  Time.deltaTime;
+        timerText.text = Mathf.Floor(timeRemaining).ToString();
+        if(timeRemaining<1)
+        {
+            SceneManager.LoadScene("EndScene");
+        }
+    }
+}
